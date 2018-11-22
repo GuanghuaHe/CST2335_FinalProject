ec8f2f7a43908e6bce3f859187ccbd49326a92e7
99rain <rainy7912@gmail.com>
2018-11-21 21:35:30 -0500

    new branch for OCT

--- app/src/main/java/com/example/guanghuahe/cst2335_finalmilestone1/OCTranspo/activity/OCRoute.java
+++ app/src/main/java/com/example/guanghuahe/cst2335_finalmilestone1/OCTranspo/activity/OCRoute.java
@@ -1,6 +1,9 @@
 package com.example.guanghuahe.cst2335_finalmilestone1.OCTranspo.activity;
+
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.ViewParent;
 import android.widget.Button;
@@ -33,12 +36,11 @@ public class OCRoute {
     private String startTime;
     private String adjustedTime;
     private String direction;
-    public List<String[]> routeList = new ArrayList<>();
+    public static final List<String[]> routeList = new ArrayList<>();
 
 
-    public static String getRouteInfo = "https://api.octranspo1.com/v1.2/GetNextTripsForStop?appID=223eb5c3&&apiKey=ab27db5b435b8c8819ffb8095328e775&stopNo=";
-    public static String getRouteInfoTrailer = "&routeNo=";
-
+    public OCRoute() {
+    }
 
     public OCRoute(String routeno, String destination, String direction, String stationNum) {
 
@@ -49,31 +51,66 @@ public class OCRoute {
 
     }
 
-    public String getRouteno() { return routeno; }
+    protected OCRoute(Parcel in) {
+        ready = in.readByte() != 0;
+        stationNum = in.readString();
+        routeno = in.readString();
+        destination = in.readString();
+        coordinates = in.readString();
+        speed = in.readString();
+        startTime = in.readString();
+        adjustedTime = in.readString();
+        direction = in.readString();
+    }
+
+
+    public String getRouteno() {
+        return routeno;
+    }
+
     public String getDestination() {
         return destination;
     }
-    public String getStationNum() { return stationNum; }
-    public String getCoordinates() { return coordinates; }
-    public String getSpeed() { return speed; }
-    public String getStartTime() { return startTime; }
-    public String getAdjustedTime() { return adjustedTime; }
-    public String getDirection() { return direction; }
 
-    public void updateData() {
-        new OCRouteQuery().execute("");
+    public String getStationNum() {
+        return stationNum;
     }
+
+    public String getCoordinates() {
+        return coordinates;
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public String getAdjustedTime() {
+        return adjustedTime;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public static void updateData(String s) {
+        new OCRoute().new OCRouteQuery().execute(s);
+        Log.e("URL ==", s);
+    }
+
 
     /**
      * uses Async to get bus details from server
-     *
      */
 
-    public class OCRouteQuery extends AsyncTask<String, Integer, String> {
+    class OCRouteQuery extends AsyncTask<String, Integer, String> {
 
         /**
-         Load the data from the OCTranspo URL
-         based on CST2335 – Graphical Interface Programming Lab 6
+         * Load the data from the OCTranspo URL
+         * based on CST2335 – Graphical Interface Programming Lab 6
          */
 
         @Override
@@ -81,10 +118,8 @@ public class OCRoute {
             Log.i("OCRoute constructor", "background activity start..");
 
             try {
-                String urlstring = getRouteInfo.concat(stationNum);
-                urlstring = urlstring.concat(getRouteInfoTrailer);
-                urlstring = urlstring.concat(routeno);
-                URL url = new URL(urlstring);
+
+                URL url = new URL(array[0]);
 
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setReadTimeout(10000 /* milliseconds */);
@@ -116,74 +151,112 @@ public class OCRoute {
                 factory.setNamespaceAware(false);
 
                 XmlPullParser xpp = factory.newPullParser();
-                    xpp.setInput(in, "UTF-8");
+                xpp.setInput(in, "UTF-8");
 
 
                 int eventType = xpp.getEventType();
 
                 //basically we cycle through the parser, we add data to our data object one piece at a time for each trip, at the end of each
                 //trip we add that trip to our result array, then reset the data object for a new trip. Until we reach the end of our XML
+                String gps = "";
+                String[] temp = null;
+                while (xpp.next() != XmlPullParser.END_DOCUMENT) {
 
-                while ((eventType != XmlPullParser.END_DOCUMENT) && cont) {
-                    String[] temp = new String[5];
-                    Log.e(" 读取信息： ", ""+xpp.getName());
-                    switch (eventType) {
-                        case XmlPullParser.START_TAG:
-                            lastTag = xpp.getName();
-                            break;
-                        case XmlPullParser.TEXT:
-                            // Starts by looking for the entry tag
-                            if (lastTag.equals("Direction") && xpp.getText().equals(direction)) {
-                                foundDirection = true;
-                            } else if (foundDirection) {
-                                Log.i("TagValue", xpp.getText());
-                                if (lastTag.equals("TripDestination"))
-                                    temp[0] = xpp.getText();
-                                else if (lastTag.equals("TripStartTime"))
-                                    temp[1] = xpp.getText();
-                                else if (lastTag.equals("AdjustedScheduleTime"))
-                                    temp[2] = xpp.getText();
-                                else if (lastTag.equals("Latitude"))
-                                    fullCoordinates = (xpp.getText().concat("/"));
-                                else if (lastTag.equals("Longitude"))
-                                    temp[3] = fullCoordinates.concat(xpp.getText());
-                                else if (lastTag.equals("GPSSpeed")) {
-                                    temp[4]= xpp.getText();
-                                }
+                    if (xpp.getEventType() != XmlPullParser.START_TAG) {
+
+
+                        if (xpp.getEventType() == XmlPullParser.END_TAG) {
+                            // if we are in an end tag, and that tag is called trip, then add the trip to our result array
+                            if (xpp.getName().equalsIgnoreCase("trip")) {
+
+                                Log.e("dada", " " + temp);
+
+
                             }
-                            routeList.add(temp);
-                            break;
-                        case XmlPullParser.END_TAG:
+                        }
 
+                        continue;
+                    } else if (xpp.getName().equalsIgnoreCase("trip")) {
+                        temp = new String[5];
+                        String name = xpp.getName();
+                        // Starts by looking for the entry tag
 
-                            /*if (xpp.getName().equals("Trip") && foundDirection) {
-                                cont = false;
-                                Log.i("Route", "breaking from parse");
-                            }*/
-                            break;
-                        default:
-                            break;
+                        if (name.equalsIgnoreCase("TripDestination")) {
+
+                            xpp.next();
+                            temp[0] = xpp.getText();
+                            Log.i("TripDestination", "Route " + temp[0]);
+
+                        }
+                        if (name.equalsIgnoreCase("TripStartTime")) {
+                            xpp.next();
+                            temp[1] = xpp.getText();
+                            Log.i("TripStartTime", "TripStartTime " + temp[1]);
+
+                        }
+
+                        if (name.equalsIgnoreCase("AdjustedScheduleTime")) {
+                            xpp.next();
+                            temp[2] = xpp.getText();
+                            Log.i("AdjustedScheduleTime", "AdjustedScheduleTime " + temp[2]);
+
+                        }
+                        if (name.equalsIgnoreCase("AdjustmentAge")) {
+                            xpp.next();
+                            String tempText = xpp.getText();
+                            Log.i("AdjustmentAge", "AdjustmentAge " + tempText);
+
+                        }
+                        if (name.equalsIgnoreCase("LastTripOfSchedule")) {
+                            xpp.next();
+                            String tempText = xpp.getText();
+                            Log.i("LastTripOfSchedule", "LastTripOfSchedule " + tempText);
+
+                        }
+
+                        if (name.equalsIgnoreCase("BusType")) {
+                            xpp.next();
+                            String tempText = xpp.getText();
+                            Log.i("BusType", "BusType " + tempText);
+
+                        }
+                        if (name.equalsIgnoreCase("GPSSpeed")) {
+                            xpp.next();
+                            temp[4] = xpp.getText();
+                            Log.i("GPSSpeed", "GPSSpeed " + temp[4]);
+
+                        }
+                        if (name.equalsIgnoreCase("Latitude")) {
+                            xpp.next();
+                            gps += xpp.getText();
+                            Log.i("Latitude", "Latitude " + gps);
+
+                        }
+                        if (name.equalsIgnoreCase("Longitude")) {
+                            xpp.next();
+                            gps += " /" + xpp.getText();
+                            Log.i("Longitude", "Longitude " + gps);
+
+                        }
+                        temp[3] = gps;
+
+                        routeList.add(temp);
                     }
-                    xpp.next();
-                    eventType = xpp.getEventType();
                 }
-                Log.i("FinalValues", destination +" "+
-                        startTime +" "+
-                        adjustedTime +" "+
-                        coordinates +" "+
-                        speed);
+
             } finally {
                 in.close();
-                Log.i("OCRoute constructor","closed input stream");
-                Log.e("OCRoute LIST DETAIL:",""+ routeList.get(0));
-                Log.e("OCRoute LIST DETAIL:",""+ routeList.get(1));
-                Log.e("OCRoute LIST DETAIL:",""+ routeList.get(2));
+                Log.i("OCRoute constructor", "closed input stream");
+                Log.e("OCRoute LIST DETAIL:", "" + routeList.get(0)[0] + "\t" + routeList.get(0)[1] + "\t" + routeList.get(0)[2] + "\t" + routeList.get(0)[3] + "\t" + routeList.get(0)[4]);
+                // Log.e("OCRoute LIST DETAIL:",""+ routeList.get(1)[0]+ "\t" + routeList.get(1)[1]+"\t"+routeList.get(1)[2]+"\t"+routeList.get(1)[3]+"\t" + routeList.get(1)[4]);
+                // Log.e("OCRoute LIST DETAIL:",""+ routeList.get(2));
             }
 
         }
+
         /**
-         *   after the async task completes
-         *   adds the data downloaded to the activity so it can be put in to the inflator and shown
+         * after the async task completes
+         * adds the data downloaded to the activity so it can be put in to the inflator and shown
          */
         @Override
         protected void onPostExecute(String result) {
@@ -196,6 +269,8 @@ public class OCRoute {
             adjustedTime = ((adjustedTime != null) ? adjustedTime : "Info NA");
             direction = ((direction != null) ? direction : "Info NA");
             ready = true;
+
+
         }
     }
 }
--- app/src/main/java/com/example/guanghuahe/cst2335_finalmilestone1/OCTranspo/adapters/DisplayRouteInfor.java
+++ app/src/main/java/com/example/guanghuahe/cst2335_finalmilestone1/OCTranspo/adapters/DisplayRouteInfor.java
@@ -18,17 +18,24 @@ import java.util.List;
  */
 
 public class DisplayRouteInfor extends Activity {
-    public static final String ACTIVITY_NAME = "DisplayRouteInfor";
 
+    public static String getRouteInfo = "https://api.octranspo1.com/v1.2/GetNextTripsForStop?appID=223eb5c3&&apiKey=ab27db5b435b8c8819ffb8095328e775&stopNo=";
+    public static String getRouteInfoTrailer = "&routeNo=";
+    public static final String ACTIVITY_NAME = "DisplayRouteInfor";
+    List<String[]> list;
 
     Button refresh;
     OCRoute route;
-    TextView routenoDestination;
-    TextView direction;
-    TextView startTime;
-    TextView adjustedTime;
-    TextView coordinates;
-    TextView speed;
+    TextView routenoDestination_0, routenoDestination_1, routenoDestination_2;
+    TextView direction_0, direction_1, direction_2;
+    TextView startTime_0, startTime_1, startTime_2;
+    TextView adjustedTime_0, adjustedTime_1, adjustedTime_2;
+    TextView coordinates_0, coordinates_1, coordinates_2;
+    TextView speed_0, speed_1, speed_2;
+    String stationNum,routeNum;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,63 +43,120 @@ public class DisplayRouteInfor extends Activity {
         setContentView(R.layout.activity_ocroute_infor);
 
 
-        speed = findViewById(R.id.speedView);
+        speed_0 = findViewById(R.id.speedView_0);
+        speed_1 = findViewById(R.id.speedView_1);
+        speed_2 = findViewById(R.id.speedView_2);
         refresh = findViewById(R.id.refreshRouteButton);
-        routenoDestination = findViewById(R.id.routenoDestinationView);
-        direction = findViewById(R.id.directionView);
-        startTime = findViewById(R.id.startTimeView);
-        adjustedTime = findViewById(R.id.adjustedTimeView);
-        coordinates =  findViewById(R.id.coordinatesView);
-        Bundle extras = getIntent().getExtras();
+        routenoDestination_0 = findViewById(R.id.routenoDestinationView_0);
+        routenoDestination_1 = findViewById(R.id.routenoDestinationView_1);
+        routenoDestination_2 = findViewById(R.id.routenoDestinationView_2);
+        direction_0 = findViewById(R.id.directionView_0);
+        direction_1 = findViewById(R.id.directionView_1);
+        direction_2 = findViewById(R.id.directionView_2);
+        startTime_0 = findViewById(R.id.startTimeView_0);
+        startTime_1 = findViewById(R.id.startTimeView_1);
+        startTime_2 = findViewById(R.id.startTimeView_2);
+        adjustedTime_0 = findViewById(R.id.adjustedTimeView_0);
+        adjustedTime_1 = findViewById(R.id.adjustedTimeView_1);
+        adjustedTime_2 = findViewById(R.id.adjustedTimeView_2);
+        coordinates_0 = findViewById(R.id.coordinatesView_0);
+        coordinates_1 = findViewById(R.id.coordinatesView_1);
+        coordinates_2 = findViewById(R.id.coordinatesView_2);
 
 
-        route = new OCRoute(extras.getString("routeno"), extras.getString("destination"),
-                extras.getString("direction"), extras.getString("stationNum")
-        );
 
-        new Update().executeOnExecutor( ((r) -> {r.run();}),"");
+      Bundle b =  getIntent().getParcelableExtra("bundle");
+            stationNum = b.getString("stationno");
+           routeNum = b.getString("routeno");
+
+
+
+
+
+
+       /* new Update().execute();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
+
+        setDisplay();
+
+
+
+
 
         //connects to a action button to get refeshed data
 
         refresh.setOnClickListener((e) -> {
             Toast toast = Toast.makeText(this, getString(R.string.oc_refresh), Toast.LENGTH_SHORT);
             toast.show();
-            route.updateData();
+            OCRoute.updateData(getRouteInfo+stationNum+getRouteInfoTrailer+routeNum);
+
             setDisplay();
         });
     }
 
     private void setDisplay() {
-        List<String[]>  list = route.routeList;
 
-        for(int i = 0; i< list.size(); i++) {
-            routenoDestination.setText(getString(R.string.oc_route) + route.getRouteno()+ " " + list.get(i)[i]);
-            direction.setText(getString(R.string.oc_direction) + route.getDirection());
-            startTime.setText(getString(R.string.oc_starttime) + route.getStartTime());
-            adjustedTime.setText(getString(R.string.oc_adjustedtime) + route.getAdjustedTime());
-            coordinates.setText(getString(R.string.oc_latlong) + route.getCoordinates());
-            speed.setText(getString(R.string.oc_gpsspeed) + route.getSpeed());
-        }
+        list = OCRoute.routeList;
+        Log.e("OCRoute LIST DETAIL:",""+ list.get(0)[0]+ "\t" + list.get(0)[1]+"\t"+list.get(0)[2]+"\t"+list.get(0)[3]+"\t" + list.get(0)[4]);
+
+        routenoDestination_0.setText(getString(R.string.oc_route) + routeNum + " " + list.get(0)[0]);
+        Log.i(ACTIVITY_NAME, " see the details!" + list.get(0)[0]);
+        routenoDestination_1.setText(getString(R.string.oc_route) + routeNum + " " + list.get(1)[0]);
+        routenoDestination_2.setText(getString(R.string.oc_route) + routeNum + " " + list.get(2)[0]);
+
+
+        direction_0.setText(getString(R.string.oc_direction) + list.get(0)[0]);
+        direction_1.setText(getString(R.string.oc_direction) + list.get(1)[0]);
+        direction_2.setText(getString(R.string.oc_direction) + list.get(2)[0]);
+
+
+        startTime_0.setText(getString(R.string.oc_starttime) + list.get(0)[1]);
+        startTime_1.setText(getString(R.string.oc_starttime) + list.get(1)[1]);
+        startTime_2.setText(getString(R.string.oc_starttime) + list.get(2)[1]);
+
+        adjustedTime_0.setText(getString(R.string.oc_adjustedtime) + list.get(0)[2]);
+        adjustedTime_1.setText(getString(R.string.oc_adjustedtime) + list.get(1)[2]);
+        adjustedTime_2.setText(getString(R.string.oc_adjustedtime) + list.get(2)[2]);
+
+
+        coordinates_0.setText(getString(R.string.oc_latlong) + list.get(0)[3]);
+        coordinates_1.setText(getString(R.string.oc_latlong) + list.get(1)[3]);
+        coordinates_2.setText(getString(R.string.oc_latlong) + list.get(2)[3]);
+
+
+        speed_0.setText(getString(R.string.oc_gpsspeed) + list.get(0)[4]);
+        speed_1.setText(getString(R.string.oc_gpsspeed) + list.get(1)[4]);
+        speed_2.setText(getString(R.string.oc_gpsspeed) + list.get(2)[4]);
+
+
+
+
     }
 
     /**
      * uses Async to get updated bus details from server
-     *
      */
     public class Update extends AsyncTask<String, Integer, String> {
         @Override
         protected String doInBackground(String... strings) {
-            route.updateData();
-            if (route.getStartTime() == null || route.getSpeed() == null || route.getCoordinates() == null || route.getAdjustedTime() == null) {
+            OCRoute.updateData(getRouteInfo+stationNum+getRouteInfoTrailer+routeNum);
+
                 try {
                     Thread.sleep(1000);
                 } catch (Exception e) {
                     Log.i(ACTIVITY_NAME, e.toString());
                 }
-                route.updateData();
-            }
-            setDisplay();
+                OCRoute.updateData(getRouteInfo+stationNum+getRouteInfoTrailer+routeNum);
+
+
             return null;
         }
+
+
     }
 }
